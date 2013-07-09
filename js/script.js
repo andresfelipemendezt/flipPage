28677179d6c3180fd3c67583a3619767e8d8b296
Andrés Felipe Méndez Torres <fmendez@existaya.com>
2013-07-09 18:11:29 +0200

    working js tag linked

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
 window.onload = function() {  
     var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
-    var circle = paper.circle(100, 100, 80);  
+    var circle = paper.circle(100, 100, 80).attr({fill: '#000'});  
 }
